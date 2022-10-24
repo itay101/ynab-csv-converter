285c01add4cb5f8aa2d48f8fd5e8706d56cbf36c
itay101 <itay101@gmail.com>
2022-10-24 20:30:55 +0300

    Added get_transactions for max processor (#1)
    Co-authored-by: Itay Roisman <itay.roisman@armis.com>

--- files_processors/isracard_processor.py
+++ files_processors/isracard_processor.py
@@ -12,6 +12,12 @@ class IsracardProcessor(RawExcelFile):
             HeaderMapItem(source="סכום חיוב", target=YnabCsvFields.OUTFLOW.value),
         ]
         self._header_mapping = [HeaderMapItem(source="תאריך רכישה", target=YnabCsvFields.DATE.value), *common_mapping]
+        self._json_mapping = {
+            YnabCsvFields.DATE_KEY.value: "תאריך רכישה",
+            YnabCsvFields.PAYEE_KEY.value: "שם בית עסק",
+            YnabCsvFields.MEMO_KEY.value: "פירוט נוסף",
+            YnabCsvFields.AMOUNT_KEY.value: "סכום חיוב",
+        }
         self._secondary_header_mapping = [HeaderMapItem(source="תאריך חיוב", target=YnabCsvFields.DATE.value),
                                           *common_mapping]
         self._body_rows = self._get_body_rows()
@@ -19,15 +25,16 @@ class IsracardProcessor(RawExcelFile):
     def _get_body_rows(self):
         rows = []
         is_secondary_table = False
-        data_frame_no_na = self._raw_data.dropna()
-        for row in data_frame_no_na.values:
-            if not is_secondary_table and self._is_main_table_row(row):
-                rows.append(self._get_main_table_row_object(row))
-            elif is_secondary_table or self._is_secondary_table(row):
-                is_secondary_table = True
-                if self._should_skip_row(row):
-                    continue
-                rows.append(self._get_secondary_table_row_object(row))
+        for sheet_name, sheet in self._raw_data.items():
+            data_frame_no_na = sheet.dropna()
+            for row in data_frame_no_na.values:
+                if not is_secondary_table and self._is_main_table_row(row):
+                    rows.append(self._get_main_table_row_object(row))
+                elif is_secondary_table or self._is_secondary_table(row):
+                    is_secondary_table = True
+                    if self._should_skip_row(row):
+                        continue
+                    rows.append(self._get_secondary_table_row_object(row))
 
         return rows
 
@@ -38,22 +45,27 @@ class IsracardProcessor(RawExcelFile):
         return row[0] == "עסקאות בחו˝ל"
 
     def _get_main_table_row_object(self, row):
+        day, month, year = self._split_date(row[0])
         return {
-            self._header_mapping[0].source: row[0],
+            self._header_mapping[0].source: self._get_ynab_date(year, month, day),
             self._header_mapping[1].source: row[1],
-            self._header_mapping[2].source: row[7] if row[7] != "nan" else "",
-            self._header_mapping[3].source: row[4],
+            self._header_mapping[2].source: row[7],
+            self._header_mapping[3].source: self._get_ynab_amount(row[4]),
         }
 
     def _get_secondary_table_row_object(self, row):
+        day, month, year = self._split_date(row[1])
         return {
-            self._header_mapping[0].source: row[1],
+            self._header_mapping[0].source: self._get_ynab_date(year, month, day),
             self._header_mapping[1].source: row[2],
             self._header_mapping[2].source: row[7],
-            self._header_mapping[3].source: row[5],
+            self._header_mapping[3].source: self._get_ynab_amount(row[5]),
         }
 
     def _should_skip_row(self, row):
         if not row[0] or row[0] == "תאריך רכישה" or row[0] == "עסקאות בחו˝ל":
             return True
         return False
+
+    def _split_date(self, value):
+        return value.split("/")
