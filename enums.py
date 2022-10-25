6bb1981cc98f42f25866f78018aaed3ecae1a9e2
itay101 <itay101@gmail.com>
2022-10-25 08:58:28 +0300

    Added JSON file as config for converter (#7)
    * Added JSON file as config for converter
    
    * Added .gitignore
    
    * Fixed comments
    
    Co-authored-by: Itay Roisman <itay.roisman@armis.com>

---
+++ enums.py
@@ -0,0 +1,23 @@
+import enum
+
+from files_processors.isracard_processor import IsracardProcessor
+from files_processors.max_processor import MaxProcessor
+from files_processors.poalim_processor import PoalimProcessor
+
+
+class AccountTypes(enum.Enum):
+    POALIM = "poalim"
+    ISRACARD = "isracard"
+    MAX = "max"
+
+
+class AccountTypeToProcessor:
+    def __init__(self):
+        self._account_type_to_processor = {
+            AccountTypes.POALIM.value: PoalimProcessor,
+            AccountTypes.ISRACARD.value: IsracardProcessor,
+            AccountTypes.MAX.value: MaxProcessor,
+        }
+
+    def get_processor_by_type(self, account_type):
+        return self._account_type_to_processor.get(account_type)
