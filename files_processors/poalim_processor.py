e360b8d38eca17a5c0da943232c42fc557406660
Itay Roisman <itay.roisman@armis.com>
2022-10-25 23:28:48 +0300

    Fixed: ynab-csv-converter-12 Add get_transactions to PoalimProcessor

--- files_processors/poalim_processor.py
+++ files_processors/poalim_processor.py
@@ -2,6 +2,9 @@ from files_processors.raw_files import HeaderMapItem
 from files_processors.raw_files import RawCSVFile
 from files_processors.raw_files import YnabCsvFields
 
+INFLOW_KEY = "זכות"
+OUTFLOW_KEY = "חובה"
+
 
 class PoalimProcessor(RawCSVFile):
     def __init__(self, **kwargs):
@@ -10,6 +13,33 @@ class PoalimProcessor(RawCSVFile):
             HeaderMapItem(source="\ufeffתאריך", target=YnabCsvFields.DATE.value),
             HeaderMapItem(source="תיאור הפעולה", target=YnabCsvFields.PAYEE.value),
             HeaderMapItem(source="אסמכתא", target=YnabCsvFields.MEMO.value),
-            HeaderMapItem(source="חובה", target=YnabCsvFields.OUTFLOW.value),
+            HeaderMapItem(source="סכום", target=YnabCsvFields.OUTFLOW.value),
             HeaderMapItem(source="זכות", target=YnabCsvFields.INFLOW.value),
         ]
+
+        self._json_mapping = {
+            YnabCsvFields.DATE_KEY.value: "\ufeffתאריך",
+            YnabCsvFields.PAYEE_KEY.value: "תיאור הפעולה",
+            YnabCsvFields.MEMO_KEY.value: "אסמכתא",
+            YnabCsvFields.AMOUNT_KEY.value: "סכום",
+        }
+
+        self._body_rows = self._get_body_rows()
+
+    def _get_body_rows(self):
+        return [self._get_row_object(row) for row in self._body_rows]
+
+    def _get_row_object(self, row):
+        year, month, day = self._split_date(row[self._json_mapping[YnabCsvFields.DATE_KEY.value].replace("\ufeff", "")])
+        outflow = row[OUTFLOW_KEY]
+        inflow = row[INFLOW_KEY]
+        return {
+            self._header_mapping[0].source: self._get_ynab_date(year, month, day),
+            self._header_mapping[1].source: row[self._json_mapping[YnabCsvFields.PAYEE_KEY.value]],
+            self._header_mapping[2].source: row[self._json_mapping[YnabCsvFields.MEMO_KEY.value]],
+            self._header_mapping[3].source: self._get_ynab_amount((float(outflow or inflow)),
+                                                                  outflow=inflow == ""),
+        }
+
+    def _split_date(self, value):
+        return value.split("-")
