878503dd643ccf2ce4577ffa45f74dc3feb8af39
Itay Roisman <itay.roisman@armis.com>
2022-12-12 23:37:38 +0200

    Added Erez Fix

--- ynab_connector_app/app/models/account.server.ts
+++ ynab_connector_app/app/models/account.server.ts
@@ -1,54 +1,62 @@
-import type { User, Note } from "@prisma/client";
+import type {User, Note, Account} from "@prisma/client";
 
-import { prisma } from "~/db.server";
+import {prisma} from "~/db.server";
 
-export type { Note } from "@prisma/client";
+export type {Note} from "@prisma/client";
 
 export function getNote({
-  id,
-  userId,
-}: Pick<Note, "id"> & {
-  userId: User["id"];
+                            id,
+                            userId,
+                        }: Pick<Note, "id"> & {
+    userId: User["id"];
 }) {
-  return prisma.note.findFirst({
-    select: { id: true, body: true, title: true },
-    where: { id, userId },
-  });
+    return prisma.note.findFirst({
+        select: {id: true, body: true, title: true},
+        where: {id, userId},
+    });
 }
 
-export function getAccountListItems({ userId }: { userId: User["id"] }) {
-  return prisma.account.findMany({
-    where: { userId },
-    select: { id: true },
-    orderBy: { updatedAt: "desc" },
-  });
+export function getAccountListItems({userId}: { userId: User["id"] }) {
+    return prisma.account.findMany({
+        where: {userId},
+        select: {id: true},
+        orderBy: {updatedAt: "desc"},
+    });
 }
 
 export function createAccount({
-  body,
-  title,
-  userId,
-}: Pick<Note, "body" | "title"> & {
-  userId: User["id"];
-}) {
-  return prisma.note.create({
-    data: {
-      title,
-      body,
-      user: {
-        connect: {
-          id: userId,
+                                  accountId,
+                                  accountIdentifier,
+                                  cardLastDigits,
+                                  password,
+                                  type,
+                                  username,
+                                  budgetId,
+                                  userId
+                              }: Account) {
+    return prisma.account.create({
+        data: {
+            accountId,
+            accountIdentifier,
+            cardLastDigits,
+            password,
+            type,
+            username,
+            budgetId,
+            user: {
+                connect: {
+                    id: userId,
+                },
+            },
         },
-      },
-    },
-  });
+    });
 }
 
 export function deleteAccount({
-  id,
-  userId,
-}: Pick<Note, "id"> & { userId: User["id"] }) {
-  return prisma.note.deleteMany({
-    where: { id, userId },
-  });
+                                  id,
+                                  userId,
+                              }: Pick<Note, "id"> & { userId: User["id"] }) {
+    return prisma.note.deleteMany({
+        where: {id, userId},
+    });
 }
--- ynab_connector_app/prisma/seed.ts
+++ ynab_connector_app/prisma/seed.ts
@@ -1,53 +1,67 @@
-import { PrismaClient } from "@prisma/client";
+import {PrismaClient} from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+    const email = "rachel@remix.run";
 
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
-  });
+    // cleanup the existing database
+    await prisma.user.delete({where: {email}}).catch(() => {
+        // no worries if it doesn't exist yet
+    });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+    const hashedPassword = await bcrypt.hash("racheliscool", 10);
 
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
+    const user = await prisma.user.create({
+        data: {
+            email,
+            password: {
+                create: {
+                    hash: hashedPassword,
+                },
+            },
         },
-      },
-    },
-  });
+    });
 
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
+    await prisma.note.create({
+        data: {
+            title: "My first note",
+            body: "Hello, world!",
+            userId: user.id,
+        },
+    });
 
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
+    await prisma.note.create({
+        data: {
+            title: "My second note",
+            body: "Hello, world!",
+            userId: user.id,
+        },
+    });
 
-  console.log(`Database has been seeded. 🌱`);
+    await prisma.account.create({
+        data:
+            {
+                accountId: "1234",
+                accountIdentifier: "abcd",
+                cardLastDigits: 1234,
+                password: "1@3$%67",
+                type: "poalim",
+                username: "kuku",
+                userId: user.id,
+                budgetId: "e02acc98-693e-4311-99c7-7b1c89ac796e"
+            }
+    });
+
+    console.log(`Database has been seeded. 🌱`);
 }
 
 seed()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
